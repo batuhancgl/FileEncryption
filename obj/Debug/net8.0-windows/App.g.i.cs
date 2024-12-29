899607ff7366e825d3d0b5e70c9d2bc60b6eeb41
batu <you@example.com>
2024-12-29 22:56:07 +0300

    first commit

--- obj/Debug/net8.0-windows/App.g.i.cs
+++ obj/Debug/net8.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace FileEncryption {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace FileEncryption {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             FileEncryption.App app = new FileEncryption.App();
             app.InitializeComponent();
